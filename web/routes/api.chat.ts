cbb453aa022336e18a40090b6ee753439c596cc2
joelriveradev <joelriveradev@gmail.com>
2024-10-01 21:43:31 -0500

    chore: add type annotations

--- web/routes/api.chat.ts
+++ web/routes/api.chat.ts
@@ -1,13 +1,13 @@
 import { ActionFunctionArgs } from '@remix-run/node'
-import { LangChainAdapter } from 'ai'
+import { LangChainAdapter, Message } from 'ai'
 import { convertToLangChainMessages } from '@/lib/ai/utils'
 import { QAChain } from '@/lib/ai/chain'
 
 export const action = async ({ request }: ActionFunctionArgs) => {
   const { messages } = await request.json()
 
-  const chat_history = convertToLangChainMessages(messages)
-  const query = messages[messages.length - 1].content
+  const chat_history = convertToLangChainMessages(messages as Message[])
+  const query: Message['content'] = messages[messages.length - 1].content
 
   const stream = await QAChain.stream({
     query,
